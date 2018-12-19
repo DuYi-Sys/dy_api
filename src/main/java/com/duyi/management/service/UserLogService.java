4e139b1b4fc4bf3f40692b0d69c37b70449839ad
yanwenyu <382246268@qq.com>
2018-12-19 17:21:03 +0800

    yan commit

--- src/main/java/com/duyi/management/service/UserLogService.java
+++ src/main/java/com/duyi/management/service/UserLogService.java
@@ -51,8 +51,8 @@ public class UserLogService {
         //邮箱内容
         StringBuffer sb = new StringBuffer();
 
-//        String yzm = "http://api.duyiedu.com/userActivate?encryptionAccount=" + encryptionAccount;
-        String yzm = "http://127.0.0.1:8080/userActivate?encryptionAccount=" + encryptionAccount;
+        String yzm = "http://api.duyiedu.com/userActivate?encryptionAccount=" + encryptionAccount;
+//        String yzm = "http://127.0.0.1:8080/userActivate?encryptionAccount=" + encryptionAccount;
 
         sb.append("<!DOCTYPE>" + "<div bgcolor='#f1fcfa'   style='border:1px solid #d9f4ee; font-size:14px; line-height:22px; color:#005aa0;padding-left:1px;padding-top:5px;   padding-bottom:5px;'><span style='font-weight:bold;'>温馨提示：</span>"
                 + "<div style='width:950px;font-family:arial;'>欢迎使用渡一教育平台，您的激活链接为：<br/><h2 style='color:green'><a href=" + yzm + ">" + yzm + "</a></h2><br/>本邮件由系统自动发出，请勿回复。<br/>感谢您的使用。<br/>XXXX有限公司</div>"
@@ -165,7 +165,7 @@ public class UserLogService {
 
     public enum UserStatusEnum {
 
-        EXIST_USER_NAME(RespStatusEnum.FAIL, "用户名已存在"), SUCCESS(RespStatusEnum.SUCCESS, "添加成功"), UNKNOW_ERROR(RespStatusEnum.FAIL, "未知错误");
+        EXIST_USER_NAME(RespStatusEnum.FAIL, "用户名已存在"), SUCCESS(RespStatusEnum.SUCCESS, "注册成功，请前往邮箱激活"), UNKNOW_ERROR(RespStatusEnum.FAIL, "未知错误");
 
         private RespStatusEnum statusEnum;
         private String msg;
